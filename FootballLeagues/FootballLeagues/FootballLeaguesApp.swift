d35da457b18039e76bf7f5bab95d959abde190bb
MohamedAENasser <mohammed_abdelnasser@yahoo.com>
2023-09-07 17:54:03 +0300

    Initial commit

---
+++ FootballLeagues/FootballLeagues/FootballLeaguesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballLeaguesApp.swift
+//  FootballLeagues
+//
+//  Created by Mohamed Abd ElNasser on 07/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FootballLeaguesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
