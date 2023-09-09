f15f8e85e5766197960b0cbf06ac17d83af5204e
MohamedAENasser <mohammed_abdelnasser@yahoo.com>
2023-09-09 18:04:21 +0300

    enhancements for network logic
    using async/await instead of closure approach for better readability

--- FootballLeagues/FootballLeagues/Network/NetworkService.swift
+++ FootballLeagues/FootballLeagues/Network/NetworkService.swift
@@ -8,50 +8,53 @@
 import Foundation
 
 protocol NetworkServiceProtocol {
-    func request<Request: DataRequestProtocol>(_ request: Request, completion: @escaping (Result<Request.Response, Error>) -> Void)
+    func request<Request: DataRequestProtocol>(_ request: Request) async -> Result<Request.Response, AppError>
 }
 
 final class NetworkService: NetworkServiceProtocol {
 
-    func request<Request: DataRequestProtocol>(_ request: Request, completion: @escaping (Result<Request.Response, Error>) -> Void) {
+    func request<Request: DataRequestProtocol>(_ request: Request) async -> Result<Request.Response, AppError> {
+        return await withCheckedContinuation { continuation in
 
-        guard var urlComponent = URLComponents(string: request.url) else {
-            return completion(.failure(ErrorResponse.invalidEndpoint))
-        }
-
-        var queryItems: [URLQueryItem] = []
-
-        request.queryItems.forEach {
-            let urlQueryItem = URLQueryItem(name: $0.key, value: $0.value)
-            queryItems.append(urlQueryItem)
-        }
-
-        urlComponent.queryItems = queryItems
-
-        guard let url = urlComponent.url else {
-            return completion(.failure(ErrorResponse.invalidEndpoint))
-        }
-
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = request.method.rawValue
-        urlRequest.allHTTPHeaderFields = request.headers
-
-        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            if let error = error {
-                return completion(.failure(error))
+            guard var urlComponent = URLComponents(string: request.url) else {
+                return continuation.resume(returning: .failure(.failedToLoadData))
             }
 
-            guard let response = response as? HTTPURLResponse, 200..<300 ~= response.statusCode,
-                let data = data else {
-                return completion(.failure(NSError()))
+            var queryItems: [URLQueryItem] = []
+
+            request.queryItems.forEach {
+                let urlQueryItem = URLQueryItem(name: $0.key, value: $0.value)
+                queryItems.append(urlQueryItem)
             }
 
-            do {
-                try completion(.success(request.decode(data)))
-            } catch let error as NSError {
-                completion(.failure(error))
+            urlComponent.queryItems = queryItems
+
+            guard let url = urlComponent.url else {
+                return continuation.resume(returning: .failure(.failedToLoadData))
             }
+
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = request.method.rawValue
+            urlRequest.allHTTPHeaderFields = request.headers
+
+            URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+                if error != nil {
+                    return continuation.resume(returning: .failure(.invalidEndpoint))
+                }
+
+                guard let response = response as? HTTPURLResponse, 200..<300 ~= response.statusCode,
+                      let data = data else {
+                    return continuation.resume(returning: .failure(.failedToLoadData))
+                }
+
+                do {
+                    try continuation.resume(returning: .success(request.decode(data)))
+                } catch  {
+                    return continuation.resume(returning: .failure(.failedToLoadData))
+                }
+            }
+            .resume()
+
         }
-        .resume()
     }
 }
